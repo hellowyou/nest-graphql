8276c33c4f5f7ba4d882e6649327f8b3752d11fb
Virnon Go2designlab <virnont.go2designlab@gmail.com>
2018-11-20 08:34:07 +0800

    Change eol back to lf

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -1,39 +1,35 @@
 export interface CreateUserInput {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
 }
 
 export interface UpdateUserInput {
-  id: string;
-  firstName?: string;
-  lastName?: string;
-  email?: string;
-  password?: string;
+    id: string;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    password?: string;
 }
 
 export interface IMutation {
-  createUser(data: CreateUserInput): User | Promise<User>;
-  updateUser(data: UpdateUserInput): User | Promise<User>;
-  deleteUser(id: string): User | Promise<User>;
+    createUser(data: CreateUserInput): User | Promise<User>;
+    updateUser(data: UpdateUserInput): User | Promise<User>;
+    deleteUser(id: string): User | Promise<User>;
 }
 
 export interface IQuery {
-  user(id: string): User | Promise<User>;
-  users(
-    first?: number,
-    after?: string,
-    skip?: number,
-  ): User[] | Promise<User[]>;
-  temp__(): boolean | Promise<boolean>;
+    user(id: string): User | Promise<User>;
+    users(first?: number, after?: string, skip?: number): User[] | Promise<User[]>;
+    temp__(): boolean | Promise<boolean>;
 }
 
 export interface User {
-  id: string;
-  firstName?: string;
-  lastName?: string;
-  email?: string;
-  createdAt?: string;
-  updatedAt?: string;
+    id: string;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    createdAt?: string;
+    updatedAt?: string;
 }
