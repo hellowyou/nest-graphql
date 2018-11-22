f10373d98b4156559b53e483e6e88fcb71fe27dc
Virnon Go2designlab <virnont.go2designlab@gmail.com>
2018-11-22 12:24:11 +0800

    remove unused files

--- src/common/index.ts
+++ src/common/index.ts
@@ -2,7 +2,6 @@ export * from './decorators';
 export * from './exceptions';
 export * from './guards';
 export * from './interceptors';
-export * from './pipes';
 export * from './constants';
 export * from './common.module';
 export * from './services';
