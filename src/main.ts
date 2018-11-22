c815fdf50e199f6cf82b161aded4cd112f63ee5e
Virnon Go2designlab <virnont.go2designlab@gmail.com>
2018-11-22 15:40:38 +0800

    Add environment on boot indicator

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 import { LoggerService } from './common';
 
 async function bootstrap() {
   const port = process.env.APP_PORT || 3000;
   const app = await NestFactory.create(AppModule);
+  const env = process.env.NODE_ENV || 'development';
+
   await app.listen(port);
 
   const logger = app.get(LoggerService);
+  logger.info(`Running on ${env} environment.`);
   logger.info(`Server running on http://localhost:${port}`);
 }
 bootstrap();
